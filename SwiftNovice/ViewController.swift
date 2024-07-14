df1ad7290f33daf9eabab07ff3a6398bad38df99
Noah Pope <npope@tutanota.com>
2024-07-14 03:56:17 -0500

    Initial Commit

---
+++ SwiftNovice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftNovice
+//
+//  Created by Noah Pope on 7/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
